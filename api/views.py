d85a811920b522f3cd63ec19b2f2f4c0c7695aac
Shloimy <shloimyelbaum@gmail.com>
2024-08-06 16:49:01 +0300

    added filter change to video by topic name

--- api/views.py
+++ api/views.py
@@ -44,7 +44,7 @@ class VideoViewSet(ModelViewSet):
 
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = {
-        "topic": ["exact"],
+        "topic": ["exact", "icontains"],
         "video_id": ["exact"],
         "subtopic": ["exact"],
         "likes": ["exact", "gte", "lte", "range"],
