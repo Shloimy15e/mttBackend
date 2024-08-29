c4843e279b05caedc7f8fd73d1dbe7b8fdb6e98c
Shloimy <shloimyelbaum@gmail.com>
2024-08-29 20:41:18 +0300

    fix for {     "error": "'Meta.fields' must not contain non-model field names: is_liked_by_user, is_saved_by_user, is_viewed_by_user" }

--- api/serializers.py
+++ api/serializers.py
@@ -75,7 +75,12 @@ class VideoSerializer(serializers.ModelSerializer):
         Meta options for the VideoSerializer class.
         """
         model = Video
-        fields = ["id", "video_id", "title", "topic", "topic_name", "subtopic", "subtopic_name", "description", "tags", "duration", "publishedAt", "likes", "userLikes", "is_liked_by_user", "views", "is_viewed_by_user", "userViews", "is_saved_by_user"]
+        fields = ["id", "video_id", "title", "topic", "topic_name", "subtopic", "subtopic_name", "description", "tags", "duration", "publishedAt", "likes", "userLikes", "views", "userViews"]
+        extra_kwargs = {
+            is_liked_by_user: {'read_only': True},
+            is_viewed_by_user: {'read_only': True},
+            is_saved_by_user: {'read_only': True}
+        }
         
     def get_userLikes(self, obj):
         """
